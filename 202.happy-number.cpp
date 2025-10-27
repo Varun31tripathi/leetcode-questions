63c4e464c8b12bb82a1d106bbf19b178d232dd5f
Varun Tripathi <varuntrip99@gmail.com>
2025-10-27 20:11:39 +0530

    ...

---
+++ 202.happy-number.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int sumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+        while (n != 1 && !seen.count(n)) {
+            seen.insert(n);
+            n = sumOfSquares(n);
+        }
+        return n == 1;
+    }
+};
