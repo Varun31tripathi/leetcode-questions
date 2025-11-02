86cd2c8bbcd6244a99849e851be907d8ead9e5ea
Varun Tripathi <varuntrip99@gmail.com>
2025-11-02 23:36:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (45.07%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> findWords(vector<string>& words) {
+        unordered_map<char, int> row = {
+            {'q',1},{'w',1},{'e',1},{'r',1},{'t',1},{'y',1},{'u',1},{'i',1},{'o',1},{'p',1},
+            {'a',2},{'s',2},{'d',2},{'f',2},{'g',2},{'h',2},{'j',2},{'k',2},{'l',2},
+            {'z',3},{'x',3},{'c',3},{'v',3},{'b',3},{'n',3},{'m',3}
+        };
+        vector<string> res;
+        for (string& w : words) {
+            int r = row[tolower(w[0])];
+            bool ok = true;
+            for (char c : w) if (row[tolower(c)] != r) { ok = false; break; }
+            if (ok) res.push_back(w);
+        }
+        return res;
+    }
+};
