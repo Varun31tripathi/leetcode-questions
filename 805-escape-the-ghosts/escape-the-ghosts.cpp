841f162983326fbf64c4913803c1514158a702ff
Varun Tripathi <varuntrip99@gmail.com>
2025-11-12 21:38:21 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (86.12%) - LeetSync

---
+++ 805-escape-the-ghosts/escape-the-ghosts.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool escapeGhosts(vector<vector<int>>& ghosts, vector<int>& target) {
+        int playerDist = abs(target[0]) + abs(target[1]);
+        for (auto& g : ghosts) {
+            int ghostDist = abs(g[0] - target[0]) + abs(g[1] - target[1]);
+            if (ghostDist <= playerDist) return false;
+        }
+        return true;
+    }
+};
