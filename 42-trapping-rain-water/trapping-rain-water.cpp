17efb98f734a8ea0529b2703139245825844c51b
Varun Tripathi <varuntrip99@gmail.com>
2025-11-16 20:39:37 +0530

    Time: 0 ms (100.00%) | Memory: 25.8 MB (99.61%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        int n = h.size();
+        int l = 0, r = n - 1;
+        int leftMax = 0, rightMax = 0, ans = 0;
+        while (l < r) {
+            if (h[l] < h[r]) {
+                if (h[l] >= leftMax) leftMax = h[l];
+                else ans += leftMax - h[l];
+                l++;
+            } else {
+                if (h[r] >= rightMax) rightMax = h[r];
+                else ans += rightMax - h[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+};
